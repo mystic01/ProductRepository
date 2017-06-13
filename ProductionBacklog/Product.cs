2dddea0cb15d1392b5f83230e1a1dbe70fc65bee
Mystic <mystic.lin@liteon.com>
2017-06-13 22:53:24 +0800

    Product 漏了 ID...

--- ProductionBacklog/Product.cs
+++ ProductionBacklog/Product.cs
@@ -2,6 +2,7 @@
 {
     public class Product
     {
+        public int Id { get; set; }
         public int Cost { get; set; }
         public int Revenue { get; set; }
         public int SellPrice { get; set; }
--- ProductionBacklogTests/ProductRepositoryTests.cs
+++ ProductionBacklogTests/ProductRepositoryTests.cs
@@ -17,17 +17,17 @@ namespace ProductRepository.Tests
         {
             var products = new List<Product>
             {
-                new Product { Cost = 1, Revenue = 11, SellPrice = 21},
-                new Product { Cost = 2, Revenue = 12, SellPrice = 22},
-                new Product { Cost = 3, Revenue = 13, SellPrice = 23},
-                new Product { Cost = 4, Revenue = 14, SellPrice = 24},
-                new Product { Cost = 5, Revenue = 15, SellPrice = 25},
-                new Product { Cost = 6, Revenue = 16, SellPrice = 26},
-                new Product { Cost = 7, Revenue = 17, SellPrice = 27},
-                new Product { Cost = 8, Revenue = 18, SellPrice = 28},
-                new Product { Cost = 9, Revenue = 19, SellPrice = 29},
-                new Product { Cost = 10, Revenue = 20, SellPrice = 30},
-                new Product { Cost = 11, Revenue = 21, SellPrice = 31},
+                new Product { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
             };
             return products;
         }
